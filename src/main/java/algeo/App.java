6d38bd507a0c6ca5af5378943e4bb02f6206825c
nayakazna <nayaka.zna@gmail.com>
2025-09-10 22:57:48 +0700

    chore: setup Maven project

---
+++ src/main/java/algeo/App.java
@@ -0,0 +1,12 @@
+package algeo;
+
+import algeo.modules.ModuleContoh;
+
+public class App {
+    public static void main(String[] args) {
+        ModuleContoh m = new ModuleContoh();
+        
+        System.out.println("Hai");
+        m.halo();
+    }
+}
